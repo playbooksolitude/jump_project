a87d9b010df488418497519bd131543bf2da1517
yohan <slatopr@gmail.com>
2023-02-27 12:01:44 +0900

    anscombe

--- 23_0227_동구밭샘플그래프.R
+++ 23_0227_동구밭샘플그래프.R
@@ -2,3 +2,49 @@
 
 #
 library(tidyverse)
+library(bbplot)
+library(patchwork)
+
+#anscombe
+anscombe
+
+# --------------------------- dataset
+tibble(
+  x1 = anscombe$x1,
+  y1 = anscombe$y1
+) -> anscombe_1
+
+tibble(
+  x1 = anscombe$x2,
+  y1 = anscombe$y2
+) -> anscombe_2
+
+tibble(
+  x1 = anscombe$x3,
+  y1 = anscombe$y3
+) -> anscombe_3
+
+tibble(
+  x1 = anscombe$x4,
+  y1 = anscombe$y4
+) -> anscombe_4
+
+anscombe_1;anscombe_1;anscombe_3;anscombe_4
+
+# --------------------------- ggplot
+anscombe_1 |> 
+  ggplot(aes(x = x1, y = y1)) + 
+  geom_point() + 
+  geom_smooth(method = "lm", se = F)
+
+anscombe_2 |> 
+  ggplot(aes(x = x1, y = y1)) + geom_point() + geom_smooth(method = "lm", se = F)
+
+anscombe_3 |> 
+  ggplot(aes(x = x1, y = y1)) + geom_point() + geom_smooth(method = "lm", se = F)
+
+anscombe_4 |> 
+  ggplot(aes(x = x1, y = y1)) + geom_point() + geom_smooth(method = "lm", se = F)
+
+
+
