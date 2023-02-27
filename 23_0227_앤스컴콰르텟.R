9edabd67db39a635df6c5eb2fb521dd6cbce4109
yohan <slatopr@gmail.com>
2023-02-27 12:20:50 +0900

    파일명 변경

--- 23_0227_앤스컴콰르텟.R
+++ 23_0227_앤스컴콰르텟.R
@@ -35,16 +35,25 @@ anscombe_1;anscombe_1;anscombe_3;anscombe_4
 anscombe_1 |> 
   ggplot(aes(x = x1, y = y1)) + 
   geom_point() + 
-  geom_smooth(method = "lm", se = F)
+  geom_smooth(method = "lm", se = F) + 
+  coord_cartesian(xlim = c(0,20), ylim = c(0,15)) -> a1
 
 anscombe_2 |> 
-  ggplot(aes(x = x1, y = y1)) + geom_point() + geom_smooth(method = "lm", se = F)
+  ggplot(aes(x = x1, y = y1)) + geom_point() + 
+  geom_smooth(method = "lm", se = F) +
+  coord_cartesian(xlim = c(0,20), ylim = c(0,15)) -> a2
 
 anscombe_3 |> 
-  ggplot(aes(x = x1, y = y1)) + geom_point() + geom_smooth(method = "lm", se = F)
+  ggplot(aes(x = x1, y = y1)) + geom_point() + 
+  geom_smooth(method = "lm", se = F) +
+  coord_cartesian(xlim = c(0,20), ylim = c(0,15)) -> a3
 
 anscombe_4 |> 
-  ggplot(aes(x = x1, y = y1)) + geom_point() + geom_smooth(method = "lm", se = F)
+  ggplot(aes(x = x1, y = y1)) + geom_point() + 
+  geom_smooth(method = "lm", se = F) +
+  coord_cartesian(xlim = c(0,20), ylim = c(0,15)) -> a4
 
+(a1 | a2) / (a3 | a4)
+ggsave("anscombe_patchwork.png")
 
 
