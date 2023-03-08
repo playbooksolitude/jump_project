d6f32027c03529d160b4b5a3d8c4ddb6dbac9179
yohan <slatopr@gmail.com>
2023-03-08 15:48:09 +0900

    updated

--- 동구밭.R
+++ 동구밭.R
@@ -2,8 +2,29 @@
 
 #
 library(tidyverse)
+library(rfm)
+library(bbplot)
 
 #
 dgb <- c("#334557", "#588195", "#CEAE88", "#EFF0F2", "#E3CFC6", "#E5B299")
 
 dgb                  
+#
+rfm_data_customer
+rfm_data_orders
+
+#user
+rfm_data_customer
+
+rfm_data_customer |> count(cut_width(number_of_orders, 10, lables = F))
+rfm_data_customer |> range(number_of_orders)
+
+#구매분포
+n_distinct(rfm_data_customer$number_of_orders)
+
+ggplot(data = rfm_data_customer, aes(x = as.factor(number_of_orders))) + 
+  geom_bar() + 
+  #geom_label(aes(label = after_stat(count)), stat = "count") +
+  bbc_style()
+
+rfm_data_customer
